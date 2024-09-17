a0e96574410762a2752c3a87c18e02469ed5a2b9
Samaira Stewart <stewartsamaira@gmail.com>
2024-09-16 22:43:39 -0400

    Practiced basic int operations

---
+++ IntegerOperations.java
@@ -0,0 +1,9 @@
+public class IntegerOperations {
+    public static void main(String[] args) {
+        int x = 3;
+        x = x * 2;
+        int result = x %= 2;
+
+        System.out.println(result);
+    }
+}

11df4299a6efafa757fd909dd6f302122e3472fe
Samaira Stewart <stewartsamaira@gmail.com>
2024-09-16 22:42:59 -0400

    Practiced concatenation excerise

---
+++ concatenation.java
@@ -0,0 +1,10 @@
+public class concatenation {
+    public static void main(String[] args){
+        String firstString = "do ";
+        String secondString = firstString.concat("re ");
+        String thirdString = firstString.concat("mi ");
+
+        System.out.println(thirdString);
+    }
+    
+}

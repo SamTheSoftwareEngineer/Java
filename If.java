32c9d93eace79cacf865163bf98c7a1ce5fcdb35
Samaira Stewart <stewartsamaira@gmail.com>
2024-09-16 22:43:29 -0400

    Practiced basic if statement in Java

---
+++ If.java
@@ -0,0 +1,8 @@
+public class If {
+    public static void main(String[] args) {
+        float temperature = 82.0f;
+        if (temperature > 0) {
+            System.out.println("Temperature is positive.");
+        }
+    }
+}

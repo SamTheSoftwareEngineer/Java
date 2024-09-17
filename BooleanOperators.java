d2f403cae8aee92cf307724a4ec758ba58f2082f
Samaira Stewart <stewartsamaira@gmail.com>
2024-09-16 22:42:44 -0400

    Completed boolean operators exercise

---
+++ BooleanOperators.java
@@ -0,0 +1,7 @@
+class BooleanOperators {
+    public static void main(String[] args) {
+        int number = 1;
+        // == will return true or false 
+        System.out.println(number != 0);
+    }
+}

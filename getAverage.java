358b4bd9e71cdfb22458edc8d9651f7ae17af950
Samaira Stewart <stewartsamaira@gmail.com>
2024-09-23 22:40:25 -0400

    Created a program that gives the average of scores

--- getAverage.java
+++ getAverage.java
@@ -1,3 +1,4 @@
+// The ... means that this method can tke a variable number of values
 public static double getAverage(double... values) {
     double sum = 0;
     int count = 0;
@@ -14,5 +15,11 @@ public static double getAverage(double... values) {
 public static void main(String[] args){
     double result = getAverage(1.1, 2.2, 3.3);
     System.out.println(result);
+
+    double result2 = getAverage(18, 6, 21.3);
+    System.out.println(result2);
+
+    double result3 = getAverage(10.5, 21.5, 31.0);
+    System.out.println(result3);
 }
 

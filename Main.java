48309652f2a066fe6e4cb6c760609aa9e611154c
Samaira Stewart <stewartsamaira@gmail.com>
2024-09-24 21:57:30 -0400

    Contains main class for different classes and their applications

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Inventory inventory = new Inventory();
+
+        Item item = new Item("Generic Item", 10);
+        Fruit fruit = new Fruit("Fuji", "Apple", 20);
+        
+        inventory.addItem(item);
+        inventory.addItem(fruit);
+
+        inventory.displayInventory();
+    }
+    
+}

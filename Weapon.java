f6d4230da73ca1fb422bceb63c4544af67ba1d16
Samaira Stewart <stewartsamaira@gmail.com>
2024-09-24 21:58:06 -0400

    Goes along with Item and Fruit classes to practice OOP

---
+++ Weapon.java
@@ -0,0 +1,10 @@
+public class Weapon extends Item {
+    private int damage;
+    private String type;
+
+    public Weapon(String name, int quantity, int damage, String type) {
+        super(name, quantity);
+        this.damage = damage;
+        this.type = type;
+    }
+}
